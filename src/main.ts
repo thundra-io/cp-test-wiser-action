6fad1381f59eda7d8f581df841edd8ffa5a853fe
yusuferdem <yerdem@catchpoint.com>
2023-07-24 11:31:27 +0300

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -63,7 +63,7 @@ async function run(): Promise<void> {
 
   function createCommentSummary(result: any): string {
     const jsonData = JSON.parse(result)
-    let commentSummary = `## Catchpoint tests that should be run according to the source code modifications`
+    let commentSummary = `## <img margin-bottom="-10px" width="40" height="40" src="https://media.licdn.com/dms/image/C560BAQEvniJjrPUgow/company-logo_200_200/0/1668188077513?e=1698278400&v=beta&t=qSx9iFeU2QI2RfDWdzFThflx5OnsiMLIf2fzzDSBlzs">  Catchpoint tests that should be run according to the source code modifications`
     for (const catchpointTest of jsonData) {
       commentSummary += `\n`
       commentSummary += `### Catchpoint Test Id: ${catchpointTest.testId}`
